01c12ec01aedea02309009b7f62b125329ffae8a
Spacemonk <dtammina@masonlive.gmu.edu>
2021-01-16 19:15:20 +0000

    Hid api key and bot token

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import json
 
 client = discord.Client()
 
+file = open('config.json',"r")
+config = json.loads(file.read())
+file.close()
 
 def get_Quote():
     response = requests.get("https://zenquotes.io/api/random")
@@ -20,7 +23,7 @@ def translate(content, src, trgt):
 
     headers = {
         'content-type': "application/json",
-        'x-rapidapi-key': 'df4a57dc9fmsh216de335b6e30ddp13e12ejsn10985c96a3ed',
+        'x-rapidapi-key': config['key'],
         'x-rapidapi-host': "deep-translate1.p.rapidapi.com"
     }
     response = requests.request("POST", url, data=payload.encode('utf-8'), headers=headers)
@@ -29,13 +32,7 @@ def translate(content, src, trgt):
     return (text)
 
 
-def change_Prefix(prefix):
-    #temp = bot_prefix
-    #bot_prefix = prefix.replace(temp + 'cp-','')
-    return (bot_prefix)
-
-
-bot_prefix = ','
+#bot_prefix = ','
 
 
 @client.event
@@ -48,26 +45,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(bot_prefix + 'quote'):
+    if message.content.startswith(config['prefix'] + 'quote'):
         quote = get_Quote()
         await message.channel.send(quote)
 
-    if message.content.startswith(bot_prefix + 'cp'):
-        await message.channel.send('Enter the new bot command')
-        change_Prefix(message)
-        await message.channel.send('The bot\'s prefix is now ' + bot_prefix)
 
-    if message.content.startswith(bot_prefix + 'tr '):
+    if message.content.startswith(config['prefix'] + 'tr '):
         src = message.content[4:6]
         trgt = message.content[7:9]
         content = message.content[10:len(message.content)]
-        await message.channel.send('Command disabled by Dev. so users get mad.. haha loser - unless you\'re the dev')
+        await message.channel.send('Translated text: '+translate(content, src, trgt))
 
 			   
 
 
 
-client.run('Nzk3OTA3MzM2MDk3NDMxNjA0.X_tTHQ.tWQYpjnp5uWyvP7kK0Us7Q7_WzE')
+client.run(config['token'])
 
 #important stuff
 
